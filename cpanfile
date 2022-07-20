03ff045dbbb3a74cea10611fed39e2a2d36a5ff0
benevolent0505 <mikio.fjt@gmail.com>
2022-07-21 02:37:30 +0900

    move purl github type

--- cpanfile
+++ cpanfile
@@ -4,6 +4,9 @@ requires 'Carton';
 requires 'Carmel';
 requires 'JSON::XS';
 requires 'URI::Escape';
+requires 'Search::Elasticsearch';
+requires 'Search::Elasticsearch::Client::2_0';
+requires 'IO::Socket::SSL';
 
 on 'develop' => sub {
     requires 'Data::Printer';
--- scripts/parse-carmel-list.pl
+++ scripts/parse-carmel-list.pl
@@ -3,6 +3,7 @@ use warnings;
 
 use JSON::XS;
 use POSIX qw(strftime);
+use Search::Elasticsearch;
 
 my $cpanfile_manifest = {};
 $cpanfile_manifest->{'cpanfile.snapshot'} = +{
@@ -25,11 +26,71 @@ while (my $line = <STDIN>) {
         };
     }
 }
+
+my $modules = [ keys %$resolved ];
+
+my $es = Search::Elasticsearch->new(
+    nodes            => 'https://fastapi.metacpan.org/v1',
+    cxn_pool         => 'Static::NoPing',
+    send_get_body_as => 'POST',
+    client           => '2_0::Direct',
+);
+
+my @must = (
+    { terms => { 'main_module' => $modules } },
+    { term => { 'resources.repository.type' => 'git' }, },
+    { term => { status                      => 'latest' } },
+    { term => { authorized                  => 'true' } },
+);
+
+my $scroller = $es->scroll_helper(
+    body        => {
+        query => {
+            bool => { must => \@must },
+        },
+    },
+    _source     => [ 'main_module', 'resources.repository' ],
+    search_type => 'scan',
+    scroll      => '5m',
+    index       => 'cpan',
+    type        => 'release',
+    size        => scalar(@$modules),
+);
+
+use DDP;
+
+while ( my $result = $scroller->next ) {
+    my $res = $result->{_source};
+
+    my $main_module = $res->{main_module};
+    my $repository = $res->{resources}->{repository};
+
+    my $namespace = '';
+    my $name = '';
+
+    my $web = $repository->{web};
+    my $url = $repository->{url};
+    if (defined $web) {
+        ($namespace, $name) = $web =~ /https:\/\/github\.com\/(.+)\/(.+)/;
+    } else {
+        ($namespace, $name) = $url =~ /git:\/\/github\.com\/(.+)\/(.+)\.git/;
+        unless ($namespace && $name) {
+            ($namespace, $name) = $url =~ /https:\/\/github\.com\/(.+)\/(.+)/;
+        }
+    }
+
+    if ($namespace && $name) {
+        $resolved->{$main_module} = {
+            package_url => sprintf("pkg:github/%s/%s", $namespace, $name),
+        };
+    }
+}
+
 $cpanfile_manifest->{'cpanfile.snapshot'}->{resolved} = $resolved;
 
-my $sha = $ENV{GITHUB_SHA};
-my $job = $ENV{GITHUB_JOB};
-my $run_id = $ENV{GITHUB_RUN_ID};
+my $sha = $ENV{GITHUB_SHA} // 'dummy_sha';
+my $job = $ENV{GITHUB_JOB} // 'dummy_job';
+my $run_id = $ENV{GITHUB_RUN_ID} // 'dummy_id';
 my $now = time();
 
 my $manifest = +{
